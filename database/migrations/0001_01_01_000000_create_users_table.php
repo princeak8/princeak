d47f1bcff556cc380b4d7e6c5b754ffadca19ba2
Akachukwu Aneke <akalodave@gmail.com>
2025-09-08 16:43:22 +0100

    updated github ci/cd

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('key', 191)->primary();
+            $table->string('id', 191)->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();

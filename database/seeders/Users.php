35bb52ab925136498951591327fccfbe2dcc285e
Akachukwu Aneke <akalodave@gmail.com>
2025-09-08 14:43:40 +0100

    Added github pipeline

---
+++ database/seeders/Users.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+use App\Enums\Role;
+
+class Users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            
+        ]; 
+    }
+}
